852c887c298daace89fbcefc5a7b976b0fceec5f
jay <1396631647@qq.com>
2021-03-29 11:41:30 +0800

    vc2修改

--- Filtering and convolution/DSPF_sp_fir_gen/vector_c/verson2.c
+++ Filtering and convolution/DSPF_sp_fir_gen/vector_c/verson2.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <math.h> 
 
-//并行化
+//并行化  这里存在不必要计算
 void DSPF_sp_fir_gen_vc2 (
     vector float *x,
            float *h,
@@ -12,18 +12,16 @@ void DSPF_sp_fir_gen_vc2 (
 ) {
 	int i,j,k;
 	int t;
-    vector float temp1,temp2,temp3,temp4,temp5,temp6;
+    vector float temp1,temp4,temp5,temp6;
     for(i = 0; i < nh; i++) {
         temp1 = vec_svbcast(h[i]);
-        temp2 = vec_svbcast(h[i]);
-        temp3 = vec_svbcast(h[i]);
 		for(j = 0,k = 0; j < nr; j+=48,k+=3) { 
-          temp4=*((vector float *)((float *)x+i+j));
-          temp5=*((vector float *)((float *)x+i+j)+1);
-          temp6=*((vector float *)((float *)x+i+j)+2);   
-          r[k]=vec_mula(temp4,temp1,r[k]);  
-          r[k+1]=vec_mula(temp5,temp2,r[k+1]); 
-          r[k+2]=vec_mula(temp6,temp3,r[k+2]); 
+            temp4=*((vector float *)((float *)x+i+j));
+            temp5=*((vector float *)((float *)x+i+j)+1);
+            temp6=*((vector float *)((float *)x+i+j)+2);   
+            r[k]=vec_mula(temp4,temp1,r[k]);  
+            r[k+1]=vec_mula(temp5,temp1,r[k+1]); 
+            r[k+2]=vec_mula(temp6,temp1,r[k+2]); 
 		}
 	}
 }
